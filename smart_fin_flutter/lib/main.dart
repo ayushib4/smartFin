0e4c1cc6f3189c5979e6646129b13f994c309001
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2023-06-17 16:45:55 -0700

    GG

--- smart_fin_flutter/lib/main.dart
+++ smart_fin_flutter/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:smart_fin_flutter/screens/home.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const SmartFin());
 }
 
--- smart_fin_flutter/lib/screens/home.dart
+++ smart_fin_flutter/lib/screens/home.dart
@@ -1,4 +1,51 @@
+// import 'package:flutter/material.dart';
+
+// class Home extends StatefulWidget {
+//   const Home({super.key});
+
+//   @override
+//   State<Home> createState() => _HomeState();
+// }
+
+// class _HomeState extends State<Home> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               const Padding(
+//                 padding: EdgeInsets.all(16.0),
+//                 child: Text(
+//                   'SmartFin',
+//                   style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+//                 child: ElevatedButton(
+//                   onPressed: () => {
+//                     print("hi"),
+//                   },
+//                   child: const Text('Sign in with Plaid'),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:plaid_flutter/plaid_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,26 +55,126 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  LinkConfiguration? _configuration;
+  StreamSubscription<LinkEvent>? _streamEvent;
+  StreamSubscription<LinkExit>? _streamExit;
+  StreamSubscription<LinkSuccess>? _streamSuccess;
+  LinkObject? _successObject;
+
+  @override
+  void initState() {
+    super.initState();
+
+    createLinkToken();
+
+    _streamEvent = PlaidLink.onEvent.listen(_onEvent);
+    _streamExit = PlaidLink.onExit.listen(_onExit);
+    _streamSuccess = PlaidLink.onSuccess.listen(_onSuccess);
+  }
+
+  Future<String> createLinkToken() async {
+    http.Response response = await http.post(
+      Uri.parse('https://sandbox.plaid.com/link/token/create'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'client_id': dotenv.env["PLAID_CLIENT_ID"].toString(),
+        'secret': dotenv.env["PLAID_SECRET"].toString(),
+        'user': {'client_user_id': 'unique-per-user'},
+        'client_name': 'SmartFin',
+        'products': ['auth', 'transactions'],
+        'country_codes': ['US'],
+        'language': 'en',
+        'account_filters': {
+          'depository': {
+            'account_subtypes': ['checking'],
+          },
+        },
+        'android_package_name': 'com.example.smart_fin_flutter',
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.body);
+    }
+
+    return response.body;
+  }
+
+  @override
+  void dispose() {
+    _streamEvent?.cancel();
+    _streamExit?.cancel();
+    _streamSuccess?.cancel();
+    super.dispose();
+  }
+
+  void _createLinkTokenConfiguration() {
+    setState(() {
+      _configuration = LinkTokenConfiguration(
+        token: "GENERATED_LINK_TOKEN",
+      );
+    });
+  }
+
+  void _onEvent(LinkEvent event) {
+    final name = event.name;
+    final metadata = event.metadata.description();
+    print("onEvent: $name, metadata: $metadata");
+  }
+
+  void _onSuccess(LinkSuccess event) {
+    final token = event.publicToken;
+    final metadata = event.metadata.description();
+    print("onSuccess: $token, metadata: $metadata");
+    setState(() => _successObject = event);
+  }
+
+  void _onExit(LinkExit event) {
+    final metadata = event.metadata.description();
+    final error = event.error?.description();
+    print("onExit metadata: $metadata, error: $error");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          width: double.infinity,
+          color: Colors.grey[200],
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'SmartFin',
-                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            children: <Widget>[
+              Expanded(
+                child: Center(
+                  child: Text(
+                    _configuration?.toJson().toString() ?? "",
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
-                child: ElevatedButton(
-                  onPressed: () => {print("hi")},
-                  child: const Text('Sign in with Plaid'),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: _createLinkTokenConfiguration,
+                child: const Text("Create Link Token Configuration"),
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: _configuration != null
+                    ? () => PlaidLink.open(configuration: _configuration!)
+                    : null,
+                child: const Text("Open"),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    _successObject?.toJson().toString() ?? "",
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
             ],
