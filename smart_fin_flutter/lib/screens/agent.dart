62b8b3badefec2d31e5a8e03ba58cb3dd1040f6d
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2023-06-18 12:20:23 -0700

    Added reactions

--- smart_fin_flutter/lib/screens/agent.dart
+++ smart_fin_flutter/lib/screens/agent.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_fin_flutter/widgets/received_message.dart';
 import 'package:video_player/video_player.dart';
 
 class Agent extends StatefulWidget {
@@ -14,6 +15,7 @@ class _AgentState extends State<Agent> {
   List messages = [];
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
+  bool visibility = true;
 
   @override
   void initState() {
@@ -37,6 +39,12 @@ class _AgentState extends State<Agent> {
     super.dispose();
   }
 
+  void hideReactions() {
+    setState(() {
+      visibility = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,9 +55,9 @@ class _AgentState extends State<Agent> {
             const Padding(
               padding: EdgeInsets.all(16.0),
               child: Text(
-                "Ask",
+                "Ask Mr. Wonderful",
                 style: TextStyle(
-                  fontSize: 48,
+                  fontSize: 36,
                   fontWeight: FontWeight.bold,
                 ),
               ),
@@ -69,37 +77,58 @@ class _AgentState extends State<Agent> {
                 }
               },
             ),
-            // AvatarGlow(
-            //   endRadius: 60.0,
-            //   child: Material(
-            //     // Replace this child with your own
-            //     elevation: 8.0,
-            //     shape: const CircleBorder(),
-            //     child: CircleAvatar(
-            //       backgroundColor: Colors.grey[100],
-            //       radius: 30.0,
-            //       child: Image.network(
-            //         'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
-            //         height: 50,
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // Padding(
-            //   padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-            //   child: ReceivedMessage(
-            //       message:
-            //           "Hey there! I noticed you spent a lot of money at Apple this month (\$1K). Can you tell me what you purchased and how necessary this purchase was?"),
-            // ),
             const Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Ask',
+              padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
+              child: ReceivedMessage(
+                  message:
+                      "Hey there! I noticed you spent a lot of money at Apple this month (\$1K)."),
+            ),
+            Visibility(
+              visible: visibility,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(32.0, 0.0, 16.0, 0.0),
+                child: Row(
+                  children: [
+                    OutlinedButton(
+                      onPressed: () => hideReactions(),
+                      child: const Icon(
+                        Icons.thumb_up,
+                        size: 20.0,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 0.0, 0.0),
+                      child: OutlinedButton(
+                        onPressed: () => hideReactions(),
+                        child: const Icon(
+                          Icons.thumb_down,
+                          size: 20.0,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 0.0, 0.0),
+                      child: OutlinedButton(
+                        onPressed: () => hideReactions(),
+                        child: const Icon(
+                          Icons.tag_faces_rounded,
+                          size: 20.0,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
+            // const Padding(
+            //   padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 16.0),
+            //   child: TextField(
+            //     decoration: InputDecoration(
+            //       border: OutlineInputBorder(),
+            //       labelText: 'Type your message here',
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
