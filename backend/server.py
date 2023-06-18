8ed75230eaf9ac5509e2ed667def8084842a9661
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2023-06-18 11:27:24 -0700

    Trivial refactors

--- backend/server.py
+++ backend/server.py
@@ -9,10 +9,11 @@ load_dotenv()
 app = Flask(__name__)
 IP_ADDRESS = getenv("IP_ADDRESS")
 
+
 @app.route("/trigger-core", methods=["POST"])
 def trigger_core_services():
     try:
-        data = request.get_json()  
+        data = request.get_json()
         user_id = data["user_id"]
         cred = credentials.Certificate("firebase-credentials.json")
         app = firebase_admin.initialize_app(cred)
@@ -20,10 +21,11 @@ def trigger_core_services():
         coll_ref = firestore_client.collection(user_id)
         docs = coll_ref.stream()
         data = [doc.to_dict() for doc in docs]
-        # insert pinecone function 
+        # insert pinecone function
         return "200"
     except Exception as e:
         return {"error": str(e)}, 400
+
 
 if __name__ == "__main__":
     app.run(host=IP_ADDRESS, debug=True)
