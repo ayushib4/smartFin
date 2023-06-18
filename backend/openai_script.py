2b1758388eaa2a970a333334fd5a8bbc895fd02a
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2023-06-17 17:23:14 -0700

    Abstract GPT fetch into a function

--- backend/openai_script.py
+++ backend/openai_script.py
@@ -6,16 +6,17 @@ openai.organization = getenv("OPENAI_ORG_ID")
 openai.api_key = getenv("OPENAI_API_KEY")
 openai.Model.list()
 
-response = openai.ChatCompletion.create(
-    model=INFERENCE_MODEL,
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Explain asynchronous programming in the style of the pirate Blackbeard.",
-        },
-    ],
-    temperature=0,
-)
 
-print(response["choices"][0]["message"]["content"])
+def fetch_gpt_response(prompt: str) -> str:
+    response = openai.ChatCompletion.create(
+        model=INFERENCE_MODEL,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        temperature=0,
+    )
+
+    return response["choices"][0]["message"]["content"]
