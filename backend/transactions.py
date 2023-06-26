f860e5b83f35ac116892fd80161a8df142833ef7
quinnouyang <90884224+quinnouyang@users.noreply.github.com>
2023-06-25 18:55:50 -0700

    Remove performance bottleneck of redundant is_indexed; trivially refactor transactions.py

--- backend/transactions.py
+++ backend/transactions.py
@@ -1,15 +1,15 @@
-import time
+from time import time
 from typing import Iterable
 import pinecone
 from os import getenv
 from data_constants import (
-    PROMPT_EXAMPLES,
     EXAMPLE_USER_ID,
     EXAMPLE_TRANSACTIONS,
 )
 from inference import InferenceModel
 from embedding import EmbeddingModel
 from dotenv import load_dotenv
+from tqdm import tqdm
 
 load_dotenv()
 
@@ -48,29 +48,23 @@ class TransactionsInterface:
         self._inference_model = inference_model
         self._embedding_model = embedding_model
 
-        if not self.is_indexed():
-            self._create_index()
+        if PINECONE_INDEX not in pinecone.list_indexes():
+            pinecone.create_index(
+                PINECONE_INDEX,
+                dimension=self._embedding_model.get_dimensions(),
+                metadata_config={"indexed": ["user_id"]},
+            )
+
+            print(f"Created Pinecone index {PINECONE_INDEX}.")
 
         self.index = pinecone.Index(PINECONE_INDEX)
 
-    def _create_index(self) -> None:
-        pinecone.create_index(
-            PINECONE_INDEX,
-            dimension=self._embedding_model.get_dimensions(),
-            metadata_config={"indexed": ["user_id"]},
-        )
-
-        print(f"Created Pinecone index.")
-
-        return
-
     def add_transactions(self, user_id: str, transactions: Iterable[dict]) -> None:
-        assert self.is_indexed()
-
-        vectors: list[tuple[str, str, dict]] = []
+        vectors: list[tuple[str, str, dict[str, str]]] = []
         count = 0
+        start_time = time()
 
-        for transaction in transactions:
+        for transaction in tqdm(transactions):
             if count > 0 and count % BATCH_SIZE == 0:
                 self.index.upsert(vectors)
                 vectors.clear()
@@ -82,18 +76,22 @@ class TransactionsInterface:
                 (
                     transaction["transaction_id"],
                     self._embedding_model.embed(transaction_str),
-                    {"user_id": user_id, "transaction": transaction_str},
+                    {
+                        "user_id": user_id,
+                        "transaction": transaction_str,
+                    },
                 )
             )
 
             count += 1
 
-        print(f"Added {count} transactions.")
+        if vectors:
+            self.index.upsert(vectors)
+
+        print(f"Added {count} transactions in {time() - start_time}s.")
 
     def _stringify_transaction(self, transaction: dict) -> str:
-        assert self.is_indexed()
-
-        strs: list[str] = []
+        field_strs: list[str] = []
 
         for field in EMBEDDABLE_FIELDS:
             if field not in transaction:
@@ -105,19 +103,17 @@ class TransactionsInterface:
             formatted_field = field.replace("_", " ").title()
 
             if field == "location":
-                strs.append(
+                field_strs.append(
                     f"{field}: {data['address']}, {data['city']}, {data['country']}, {data['postal_code']}."
                 )
             elif field == "personal_finance_category":
-                strs.append(f"{formatted_field}: {data['detailed']}.")
+                field_strs.append(f"{formatted_field}: {data['detailed']}.")
             else:
-                strs.append(f"{formatted_field}: {data}.")
+                field_strs.append(f"{formatted_field}: {data}.")
 
-        return " ".join(strs)
+        return " ".join(field_strs)
 
     def query(self, user_id: str, query: str, num_results=5) -> list[dict[float, str]]:
-        assert self.is_indexed()
-
         query_embedding = self._embedding_model.embed(query)
 
         responses = self.index.query(
@@ -129,22 +125,23 @@ class TransactionsInterface:
 
         return [{r["score"]: f"{r['metadata']['transaction']}"} for r in responses]  # type: ignore
 
-    def is_indexed(self) -> bool:
-        return PINECONE_INDEX in pinecone.list_indexes()
-
 
 if __name__ == "__main__":
-    print("Start " + str(time.time()))
+    start_time = time()
 
     transactions_interface = TransactionsInterface(
-        InferenceModel(prompt_examples=PROMPT_EXAMPLES),
+        InferenceModel(),
         EmbeddingModel(),
     )
 
+    print(f"Initialized TransactionsInterface in {time() - start_time}s.")
+
     transactions_interface.add_transactions(EXAMPLE_USER_ID, EXAMPLE_TRANSACTIONS)
-    print("Done storing " + str(time.time()))
+
     response = transactions_interface.query(
         EXAMPLE_USER_ID, "What is my most stupid purchase?"
     )
+
     print(response)
-    print("Done querying " + str(time.time()))
+
+    print(f"Completed test in {time() - start_time}s.")
